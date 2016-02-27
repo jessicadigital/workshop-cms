f89ee88e91cded98933744f5cd998c6075adc58b
Jessica Muirhead <github@jessica.digital>
2016-02-27 12:28:18 +0000

    Return index.html

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 var http = require('http');
+var fs = require('fs');
 
 var message = 'I am so happy to be part of the Node Girls workshop';
 
 function handler(request, response) {
+	var endpoint = request.url;
+	console.log(endpoint);
+
+	var method = request.method;
+	console.log(method);
+
 	response.writeHead(200, {"Content-Type": "text/html"});
- 	response.write(message); //response body
-  	response.end(); // finish response
+	if (endpoint === '/') {
+		fs.readFile(__dirname+'/public/index.html', function(error, file) {
+			if (error) {
+				console.log(error);
+				return;
+			}
+			response.end(file);
+		});
+	}
+	else {
+		if (endpoint === '/node') {
+			response.write("We <3 Node!");
+		}
+		else if (endpoint === '/girls') {
+			response.write('We are girls!');
+		}
+		else {
+	 		response.write(message); //response body
+		}
+	  	response.end(); // finish response
+	}
 }
 
 var server = http.createServer(handler);

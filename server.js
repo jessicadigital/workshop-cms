2c42c9a6b9025dd708528603553a2ceec1590eec
marsty5 <marsty5@gmail.com>
2016-02-27 12:53:54 +0000

    Respond to requests for css or images accrodingly

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ function handler(request, response) {
 	var method = request.method;
 	console.log(method);
 
-	response.writeHead(200, {"Content-Type": "text/html"});
+	/* First if should be a control check for hackers who want to look at our files
+	or stupid people who don't know how to search :p */
 	if (endpoint === '/') {
+		response.writeHead(200, {"Content-Type": "text/html"});
 		fs.readFile(__dirname+'/public/index.html', function(error, file) {
 			if (error) {
 				console.log(error);
@@ -20,17 +22,31 @@ function handler(request, response) {
 			response.end(file);
 		});
 	}
+	else if (endpoint === '/node') {
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("We <3 Node!");
+		response.end(); // finish response
+		// Alternative: response.end("We <3 Node!");
+	}
+	else if (endpoint === '/girls') {
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write('We are girls!');
+  	response.end(); // finish response
+	}
 	else {
-		if (endpoint === '/node') {
-			response.write("We <3 Node!");
+		if (endpoint.indexOf('.css') > -1) {
+			response.writeHead(200, {"Content-Type": "text/css"});
 		}
-		else if (endpoint === '/girls') {
-			response.write('We are girls!');
+		if (endpoint.indexOf('.jpg') > -1) {
+			response.writeHead(200, {"Content-Type": "image/jpg"});
 		}
-		else {
-	 		response.write(message); //response body
-		}
-	  	response.end(); // finish response
+		fs.readFile(__dirname + '/public' + endpoint, function(error, file) {
+			if (error) {
+				console.log(error);
+				return;
+			}
+			response.end(file); // finish response
+		});
 	}
 }
 
